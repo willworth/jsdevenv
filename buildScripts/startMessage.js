9c9077ab6663a1c9e5f3f4bef6f7b10ea728ac5e
will <code@willworth.co.uk>
2017-03-20 09:23:25 +0100

    add babel

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
-var chalk = require('chalk');
-
+import chalk from 'chalk';  // es6 style, used to test if babel transpiling correctly
+//chalk allows you to specify colour of output to the console.
 console.log(chalk.green("Starting app in dev mode..."));
