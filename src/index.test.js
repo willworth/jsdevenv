b7e8a30ae2316823c7950a8cd13fd924a1082b8a
will <code@willworth.co.uk>
2017-03-21 14:15:44 +0100

    travis test

--- src/index.test.js
+++ src/index.test.js
@@ -11,11 +11,12 @@ describe('Our first test', () =>{
 
 
 describe('index.html', () =>{
-  it('should say hello', () =>{
+  it('should say hello', (done) =>{
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
       expect(h1.innerHTML).to.equal("Hello World!");
+      done();
       window.close();//free up the memory
     });
   })
