f77f78ff5bf14bf0a4f2f93d4aa11c87ec08aec4
will <code@willworth.co.uk>
2017-03-21 11:02:15 +0100

    add linting

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -5,6 +5,8 @@ import webpack from "webpack";
 import config from '../webpack.config.dev';
 
 
+
+/* eslint-disable no-console*/
 const port = 3000;
 const app = express();
 const complier = webpack(config);
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from 'chalk';  // es6 style, used to test if babel transpiling correctly
 //chalk allows you to specify colour of output to the console.
-console.log(chalk.green("Starting app in dev mode..."));
+console.log(chalk.green("Starting app in dev mode...")); // eslint-disable-line no-console
